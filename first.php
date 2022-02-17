ecba6ebc790949d7ef7d3e9cde5d6c8bd3dfc304
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2022-02-17 16:20:15 +0530

    Added 2nd line

--- first.php
+++ first.php
@@ -1,15 +1,3 @@
 <?php
-
-
-    echo "Hello";
-    echo "Hello 2";
-
-    echo "third change";
-    echo "fourth change";
-    echo "5th change"
-    echo "6th change"
-    
-    // New comment
-
-
+    echo "1st line"
 ?>
